443c8f00be8ea259b339440b9573bceecf0f62ec
ThiagoFelipe86 <91923154+ThiagoFelipe86@users.noreply.github.com>
2023-04-05 20:09:46 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function initMap() {
+		  
+		  const map = new google.maps.Map(document.getElementById("map"), {
+			
+			center: { lat: -7.0251456, lng: -34.8309231 },
+			zoom: 19,
+			 heading: 280,
+			mapId: "5ee54e9993f76cb4"
+		  });
+		
+				
+		
+		
+		
+		}
+		window.initMap = initMap;
